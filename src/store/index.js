f183f2251962cdcc791c28c1206e82e0a7c07a1c
Daniel Aguilar <dyangeltk17@gmail.com>
2022-09-27 20:21:58 -0500

    :sparkles: Add login view

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,11 @@
 import { createStore, createLogger } from "vuex";
-import cart from "./modules/cart";
-import products from "./modules/products";
+// import cart from "./modules/cart";
+import products from "./modules/product";
 
 const debug = process.env.NODE_ENV !== "production";
 
 export default createStore({
   modules: {
-    cart,
     products,
   },
   strict: debug,
