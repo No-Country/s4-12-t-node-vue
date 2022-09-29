81491b35c17e92148a113942931e609b7848572d
Onnichan <dyangeltk17@gmail.com>
2022-09-29 16:47:36 -0500

    :recycle: Add login redirection to dashboard

--- src/views/ScLoginView.vue
+++ src/views/ScLoginView.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { ref, watch } from "vue";
+import { ref } from "vue";
+import { useRouter } from "vue-router";
 import ScInputIcon from "../components/Input/ScInputIcon.vue";
 import ScMailIcon from "../components/icons/ScMailIcon.vue";
 import ScInput from "../components/Input/ScInput.vue";
@@ -7,25 +8,33 @@ import ScLockIcon from "../components/icons/ScLockIcon.vue";
 import ScButton from "../components/Button/ScButton.vue";
 import ScText from "../components/Text/ScText.vue";
 
-// const messageValidation = ref("");
-const email = ref("");
+const router = useRouter();
+const emailValidationAlertMessage = ref("");
+const passwordValidationAlertMessage = ref("");
+// const email = ref("");
 // const password = ref("");
-// const handleVaditation = (e) => {};
-watch(email, (newEmail) => {
-  console.log(newEmail);
-});
+const emailValidationHandler = (e) => {
+  emailValidationAlertMessage.value = e.target.value;
+};
+
+const submitHandler = () => {
+  router.push({ name: "dashboard" });
+};
 </script>
 <template>
-  <form class="border border-gray-700 w-4/12 px-4 py-5 rounded-md m-auto">
+  <form
+    class="border border-gray-700 w-4/12 px-4 py-5 rounded-md m-auto mt-48"
+    @submit.prevent="submitHandler"
+  >
     <ScText tag="h1" class="font-bold text-2xl mb-5">Login</ScText>
     <ScInputIcon>
       <template #input>
         <ScInput
           class="pl-8"
           type="email"
-          v-model="email"
-          :value="email"
+          v-model.lazy="emailValidationHandler"
           placeholder="dyangeltk17@gmail.com"
+          value="dyangeltk17@gmail.com"
           required
         />
       </template>
@@ -37,12 +46,12 @@ watch(email, (newEmail) => {
         />
       </template>
     </ScInputIcon>
-    <ScText class="text-sm text-red-500 mb-4 block"
-      >Hay un error con el email</ScText
-    >
+    <ScText class="text-sm text-red-500 mb-4 block">{{
+      emailValidationAlertMessage
+    }}</ScText>
     <ScInputIcon>
       <template #input>
-        <ScInput class="pl-8" type="password" />
+        <ScInput class="pl-8" type="password" value="123456" />
       </template>
       <template #icon>
         <ScLockIcon
@@ -52,9 +61,9 @@ watch(email, (newEmail) => {
         />
       </template>
     </ScInputIcon>
-    <ScText class="text-sm text-red-500 mb-4 block"
-      >Hay un error con el password</ScText
-    >
+    <ScText class="text-sm text-red-500 mb-4 block">{{
+      passwordValidationAlertMessage
+    }}</ScText>
     <ScButton type="submit">Iniciar sesion</ScButton>
   </form>
 </template>
