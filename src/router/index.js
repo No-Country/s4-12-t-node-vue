0c74cbf4e9f870a9d4132e1dc671f1831bdc9010
Daniel Aguilar <dyangeltk17@gmail.com>
2022-10-06 14:12:05 -0500

    :recycle: Refactor the router for dashboard child routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/ScHomeView.vue";
+import Home from "../views/ScHomeView.vue";
+import CondominiumDashboard from "../views/Dashboard/ScCondominiumDashboardView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +8,7 @@ const router = createRouter({
     {
       path: "/",
       name: "home",
-      component: HomeView,
+      component: Home,
     },
     {
       path: "/login",
@@ -20,9 +21,21 @@ const router = createRouter({
       component: () => import("../views/ScRegisterView.vue"),
     },
     {
-      path: "/Dashboard",
+      path: "/dashboard",
       name: "Dashboard",
-      component: () => import("../views/ScDashboardView.vue"),
+      component: () => import("../views/Dashboard/ScDashboardView.vue"),
+      children:[
+        {
+          path:'',
+          name: 'index',
+          component: CondominiumDashboard,
+        },
+        {
+          path:'condominium',
+          name: 'condominium',
+          component: () => import("../views/Dashboard/ScCondominiumDashboardView.vue"),
+        }
+      ]
     },
   ],
 });
